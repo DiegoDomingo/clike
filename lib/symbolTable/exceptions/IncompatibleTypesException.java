87ddfa23a99a845c9b4540b9c1778c89a1e33efc
PabloCervera <114725912+PabloCervera@users.noreply.github.com>
2023-03-30 11:25:26 +0200

    Create IncompatibleTypesException.java

---
+++ lib/symbolTable/exceptions/IncompatibleTypesException.java
@@ -0,0 +1,18 @@
+/*********************************************************************************
+ * Excepción utilizada al encontrar tipos incompatibles en una instrucción
+ *
+ * Fichero:    AlreadyDefinedSymbolException.java
+ * Fecha:      30/03/2023
+ * Versión:    v1.1
+ * Asignatura: Procesadores de Lenguajes, curso 2022-2023
+ **********************************************************************************/
+
+package lib.symbolTable.exceptions;
+
+public class IncompatibleTypesException extends Exception {
+
+    public IncompatibleTypesException() {
+
+    }
+    
+}
